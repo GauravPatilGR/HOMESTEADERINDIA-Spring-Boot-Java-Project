fc07a53be7402c52d8c529e65a46fb788d042bf2
Gaurav <Gaurav@GauravPatil>
2024-02-03 10:45:22 +0530

    war file Generated

--- src/main/java/com/example/demo/CravitaProjectHomeSteaderApplication.java
+++ src/main/java/com/example/demo/CravitaProjectHomeSteaderApplication.java
@@ -2,9 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class CravitaProjectHomeSteaderApplication {
+public class CravitaProjectHomeSteaderApplication extends SpringBootServletInitializer
+{
+	
+	
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(CravitaProjectHomeSteaderApplication.class);  
+	}  
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CravitaProjectHomeSteaderApplication.class, args);
